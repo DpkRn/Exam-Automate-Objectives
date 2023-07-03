92c5ebb8ffa3af9a564886b1c0de3fbbff27e01c
DpkRn <dwizardtechno@gmail.com>
2023-07-03 18:39:09 +0530

    script added !

---
+++ HITBULLS/Script.js
@@ -0,0 +1,16 @@
+let keys = "CABDAAAAACDAADBABAABCAABDBABBCDABCCBCDAD";
+let len = keys.length;
+
+let i = 0;
+
+const inter = setInterval(
+  function(){
+    document.querySelector(`#${keys[i]}_${i+1}`).click();
+    document.querySelector("#main_div > div.tableWidthPercent > div.onlineTestLeftDiv > div.qnav > span.saveNextButton > a").click();
+    i++;
+    
+    if(i == len){
+      clearInterval(inter)
+    }
+  },
+2000);
